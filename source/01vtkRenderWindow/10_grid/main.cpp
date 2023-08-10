0176d7056642e85c05ce133caebf34782d97b9c9
worker-Windows <yangpan0822@qq.com>
2023-08-10 14:51:12 +0800

    CGNS读取，FLUENT文件读取

--- source/01vtkRenderWindow/10_grid/main.cpp
+++ source/01vtkRenderWindow/10_grid/main.cpp
@@ -5,16 +5,17 @@
 /*
  * 1. 结构cgns
  * 2. 非结构cgns
- * 3.
+ * 3. FLUENT *.cas *.dat 只绘制图形，不映射流场数据 FLUENT控制台使用 f c n wcd fileName 导出 fileName.cas和fileName.dat
+ * 4. FLUENT *.cas *.dat 将流场数据映射到颜色
+ * 
  */
 
-#define TEST1
+#define TEST4
 
 #ifdef TEST1
 
 #include <vtkActor.h>
 #include <vtkCGNSReader.h>
-#include <vtkCamera.h>
 #include <vtkCellData.h>
 #include <vtkDataSetMapper.h>
 #include <vtkInteractorStyleTrackballCamera.h>
@@ -45,51 +46,25 @@ int main(int argc, char* argv[])
 
     std::cout << "number of blocks: " << nb << '\n';
 
+    vtkNew<vtkRenderer> renderer;
     for (unsigned int i = 0; i < nb; ++i)
     {
         auto mb2 = vtkMultiBlockDataSet::SafeDownCast(mb->GetBlock(i));
         for (unsigned int j = 0; j < mb2->GetNumberOfBlocks(); ++j)
         {
-            auto ug = vtkStructuredGrid::SafeDownCast(mb2->GetBlock(j));
-            auto np = ug->GetNumberOfPoints();
-            auto nc = ug->GetNumberOfCells();
+            auto sg = vtkStructuredGrid::SafeDownCast(mb2->GetBlock(j));
+            std::cout << "------------------------------------------------------------\n=== Block ===\t(" << i << "," << j << ")\t=== Block ===\n";
+            sg->Print(std::cout);
 
-            auto cd = ug->GetCellData();
-            auto pd = ug->GetPointData();
+            vtkNew<vtkDataSetMapper> mapper;
+            mapper->SetInputData(sg);
+            vtkNew<vtkActor> actor;
+            actor->SetMapper(mapper);
 
-            auto cdScalars  = cd->GetScalars();
-            auto cdVectors  = cd->GetVectors();
-            auto cdTensors  = cd->GetTensors();
-            auto cdTCoords  = cd->GetTCoords();
-            auto cdTangents = cd->GetTangents();
-            auto cdNormals  = cd->GetNormals();
-
-            auto pdScalars  = pd->GetScalars();
-            auto pdVectors  = pd->GetVectors();
-            auto pdTensors  = pd->GetTensors();
-            auto pdTCoords  = pd->GetTCoords();
-            auto pdTangents = pd->GetTangents();
-            auto pdNormals  = pd->GetNormals();
-
-            mapper->SetInputData(ug);
-
-            std::cout << "number of cells: " << nc << "\nnumber of points: " << np << '\n';
-
-            for (vtkIdType k = 0; k < ug->GetNumberOfCells(); ++k)
-            {
-                vtkCell* cell = ug->GetCell(k);
-            }
+            renderer->AddActor(actor);
         }
     }
 
-    mapper->ScalarVisibilityOn();
-
-    vtkNew<vtkActor> actor;
-    actor->SetMapper(mapper);
-    // actor->GetProperty()->SetColor(1, 0, 0);
-
-    vtkNew<vtkRenderer> renderer;
-    renderer->AddActor(actor);
     renderer->SetBackground(.1, .2, .3);
     renderer->ResetCamera();
 
@@ -115,7 +90,6 @@ int main(int argc, char* argv[])
 
 #include <vtkActor.h>
 #include <vtkCGNSReader.h>
-#include <vtkCamera.h>
 #include <vtkCellData.h>
 #include <vtkDataSetMapper.h>
 #include <vtkInteractorStyleTrackballCamera.h>
@@ -146,51 +120,25 @@ int main(int argc, char* argv[])
 
     std::cout << "number of blocks: " << nb << '\n';
 
+    vtkNew<vtkRenderer> renderer;
     for (unsigned int i = 0; i < nb; ++i)
     {
         auto mb2 = vtkMultiBlockDataSet::SafeDownCast(mb->GetBlock(i));
         for (unsigned int j = 0; j < mb2->GetNumberOfBlocks(); ++j)
         {
             auto ug = vtkUnstructuredGrid::SafeDownCast(mb2->GetBlock(j));
-            auto np = ug->GetNumberOfPoints();
-            auto nc = ug->GetNumberOfCells();
+            std::cout << "------------------------------------------------------------\n=== Block ===\t(" << i << "," << j << ")\t=== Block ===\n";
+            ug->Print(std::cout);
 
-            auto cd = ug->GetCellData();
-            auto pd = ug->GetPointData();
-
-            auto cdScalars  = cd->GetScalars();
-            auto cdVectors  = cd->GetVectors();
-            auto cdTensors  = cd->GetTensors();
-            auto cdTCoords  = cd->GetTCoords();
-            auto cdTangents = cd->GetTangents();
-            auto cdNormals  = cd->GetNormals();
-
-            auto pdScalars  = pd->GetScalars();
-            auto pdVectors  = pd->GetVectors();
-            auto pdTensors  = pd->GetTensors();
-            auto pdTCoords  = pd->GetTCoords();
-            auto pdTangents = pd->GetTangents();
-            auto pdNormals  = pd->GetNormals();
-
+            vtkNew<vtkDataSetMapper> mapper;
             mapper->SetInputData(ug);
+            vtkNew<vtkActor> actor;
+            actor->SetMapper(mapper);
 
-            std::cout << "number of cells: " << nc << "\nnumber of points: " << np << '\n';
-
-            for (vtkIdType k = 0; k < ug->GetNumberOfCells(); ++k)
-            {
-                vtkCell* cell = ug->GetCell(k);
-            }
+            renderer->AddActor(actor);
         }
     }
 
-    mapper->ScalarVisibilityOn();
-
-    vtkNew<vtkActor> actor;
-    actor->SetMapper(mapper);
-    // actor->GetProperty()->SetColor(1, 0, 0);
-
-    vtkNew<vtkRenderer> renderer;
-    renderer->AddActor(actor);
     renderer->SetBackground(.1, .2, .3);
     renderer->ResetCamera();
 
@@ -211,3 +159,175 @@ int main(int argc, char* argv[])
 }
 
 #endif // TEST2
+
+#ifdef TEST3
+
+#include <iostream>
+#include <vtkActor.h>
+#include <vtkCellData.h>
+#include <vtkDataSetMapper.h>
+#include <vtkFLUENTReader.h>
+#include <vtkFieldData.h>
+#include <vtkInteractorStyleTrackballCamera.h>
+#include <vtkMultiBlockDataSet.h>
+#include <vtkPointData.h>
+#include <vtkProperty.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkRenderer.h>
+#include <vtkSmartPointer.h>
+#include <vtkUnstructuredGrid.h>
+
+int main()
+{
+    // *.dat必须和*.cas在同一路径下，且前缀相同（文件名相同）
+    const char* fileName = "../resources/sqnz_s.adf.cas";
+    vtkNew<vtkFLUENTReader> fluent;
+    fluent->SetFileName(fileName);
+    fluent->EnableAllCellArrays();
+    fluent->Update();
+
+    auto dataSet   = fluent->GetOutput();
+    auto numBlocks = dataSet->GetNumberOfBlocks();
+
+    std::cout << "NumOfBlocks: " << numBlocks << '\n';
+
+    vtkNew<vtkRenderer> renderer;
+    for (uint32_t i = 0; i < numBlocks; ++i)
+    {
+        auto usg = vtkUnstructuredGrid::SafeDownCast(dataSet->GetBlock(i));
+        std::cout << "---------------------------------------------\n=== Block " << i << " ===\n";
+        usg->Print(std::cout);
+
+        vtkNew<vtkDataSetMapper> mapper;
+        mapper->SetInputData(usg);
+
+        vtkNew<vtkActor> actor;
+        actor->SetMapper(mapper);
+
+        renderer->AddActor(actor);
+    }
+
+    renderer->SetBackground(.1, .2, .3);
+    renderer->ResetCamera();
+
+    vtkNew<vtkRenderWindow> renderWindow;
+    renderWindow->AddRenderer(renderer);
+
+    vtkNew<vtkRenderWindowInteractor> renderWindowInteractor;
+    renderWindowInteractor->SetRenderWindow(renderWindow);
+
+    vtkNew<vtkInteractorStyleTrackballCamera> style;
+    renderWindowInteractor->SetInteractorStyle(style);
+
+    renderWindow->SetSize(800, 600);
+    renderWindow->Render();
+    renderWindowInteractor->Start();
+
+    return EXIT_SUCCESS;
+}
+
+#endif // TEST3
+
+#ifdef TEST4
+
+#include <iostream>
+#include <vtkActor.h>
+#include <vtkCellData.h>
+#include <vtkDataSetMapper.h>
+#include <vtkFLUENTReader.h>
+#include <vtkFieldData.h>
+#include <vtkInteractorStyleTrackballCamera.h>
+#include <vtkLookupTable.h>
+#include <vtkMultiBlockDataSet.h>
+#include <vtkPointData.h>
+#include <vtkProperty.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkRenderer.h>
+#include <vtkScalarBarActor.h>
+#include <vtkSmartPointer.h>
+#include <vtkUnstructuredGrid.h>
+
+/*
+ *   Array 0 name = PRESSURE
+ *   Array 1 name = X_VELOCITY
+ *   Array 2 name = Y_VELOCITY
+ *   Array 3 name = Z_VELOCITY
+ *   Array 4 name = TKE
+ *   Array 5 name = SDR
+ *   Array 6 name = DENSITY
+ *   Array 7 name = MU_LAM
+ *   Array 8 name = MU_TURB
+ *   Array 9 name = WALL_DIST
+ *   Array 10 name = BODY_FORCES
+ */
+
+int main()
+{
+    // *.dat必须和*.cas在同一路径下，且前缀相同（文件名相同）
+    const char* fileName = "../resources/sqnz_s.adf.cas";
+    vtkNew<vtkFLUENTReader> fluent;
+    fluent->SetFileName(fileName);
+    fluent->EnableAllCellArrays();
+    fluent->Update();
+
+    auto dataSet   = fluent->GetOutput();
+    auto numBlocks = dataSet->GetNumberOfBlocks();
+
+    std::cout << "NumOfBlocks: " << numBlocks << '\n';
+
+    vtkNew<vtkLookupTable> lut;
+    lut->SetNumberOfTableValues(20);
+    lut->SetHueRange(0.67, 0);
+    lut->SetRange(-3.6, 0.69);
+    lut->Build();
+
+    vtkNew<vtkScalarBarActor> scalarBar;
+    scalarBar->SetLookupTable(lut);
+
+    vtkNew<vtkRenderer> renderer;
+    renderer->AddActor(scalarBar);
+
+    for (uint32_t i = 0; i < numBlocks; ++i)
+    {
+        auto usg = vtkUnstructuredGrid::SafeDownCast(dataSet->GetBlock(i));
+        std::cout << "---------------------------------------------\n=== Block " << i << " ===\n";
+
+        auto tmp_range = usg->GetCellData()->GetArray("PRESSURE")->GetRange();
+        std::cout << "Range: " << tmp_range[0] << '\t' << tmp_range[1] << '\n';
+
+        vtkNew<vtkDataSetMapper> mapper;
+        mapper->SetInputData(usg);
+        mapper->SetLookupTable(lut);
+        mapper->ScalarVisibilityOn();
+        mapper->SetScalarModeToUseCellFieldData();
+        mapper->SelectColorArray("PRESSURE");
+        mapper->SetScalarRange(-3.6, 0.69);
+
+        vtkNew<vtkActor> actor;
+        actor->SetMapper(mapper);
+
+        renderer->AddActor(actor);
+    }
+
+    renderer->SetBackground(.1, .2, .3);
+    renderer->ResetCamera();
+
+    vtkNew<vtkRenderWindow> renderWindow;
+    renderWindow->AddRenderer(renderer);
+
+    vtkNew<vtkRenderWindowInteractor> renderWindowInteractor;
+    renderWindowInteractor->SetRenderWindow(renderWindow);
+
+    vtkNew<vtkInteractorStyleTrackballCamera> style;
+    renderWindowInteractor->SetInteractorStyle(style);
+
+    renderWindow->SetSize(800, 600);
+    renderWindow->Render();
+    renderWindowInteractor->Start();
+
+    return EXIT_SUCCESS;
+}
+
+#endif // TEST4
