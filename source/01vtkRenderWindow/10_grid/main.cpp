8a67d87a59040227971897538163109abcb28a44
worker-Windows <yangpan0822@qq.com>
2023-11-21 15:59:22 +0800

    vtk自带的圆、箭头，vtkPolyData转vtkUnsGrid

--- source/01vtkRenderWindow/10_grid/main.cpp
+++ source/01vtkRenderWindow/10_grid/main.cpp
@@ -18,7 +18,7 @@
  * 402. vtkPolyData 设置多种单元(line poly...)，获取单元的索引
  */
 
-#define TEST304
+#define TEST401
 
 #ifdef TEST101
 
@@ -740,16 +740,360 @@ int main()
 // vtkStructuredGrid 官方示例
 // https://kitware.github.io/vtk-examples/site/Cxx/StructuredGrid/SGrid/
 
+// 13_filter TEST601 vtkDataSetSurfaceFilter      将 vtkUnstructuredGrid 转换为 vtkPolyData
+// 13_filter TEST603 vtkDataSetToDataObjectFilter 将数据集(vtkDataSet)转换为数据对象(vtkDataObject)，然后写入文本文件
+
+// vtk9.1还没有实现 vtkPolyDataToUnstructuredGrid 9.3已经实现
+
 #include <iostream>
+#include <vtkActor.h>
+#include <vtkCellArray.h>
+#include <vtkCellData.h>
+#include <vtkDataSetMapper.h>
+#include <vtkInformation.h>
+#include <vtkInformationVector.h>
+#include <vtkInteractorStyleTrackballCamera.h>
+#include <vtkObjectFactory.h>
+#include <vtkPointData.h>
+#include <vtkPolyData.h>
+#include <vtkProperty.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkRenderer.h>
+#include <vtkSMPTools.h>
+#include <vtkSmartPointer.h>
+#include <vtkUnstructuredGrid.h>
+#include <vtkUnstructuredGridAlgorithm.h>
+
+class vtkPolyDataToUnstructuredGrid : public vtkUnstructuredGridAlgorithm
+{
+public:
+    static vtkPolyDataToUnstructuredGrid* New();
+    vtkTypeMacro(vtkPolyDataToUnstructuredGrid, vtkUnstructuredGridAlgorithm);
+    void PrintSelf(ostream& os, vtkIndent indent) override;
+
+    static bool CanBeProcessedFast(vtkPolyData* polyData);
+
+protected:
+    vtkPolyDataToUnstructuredGrid();
+    ~vtkPolyDataToUnstructuredGrid() override;
+
+    int FillInputPortInformation(int port, vtkInformation* info) override;
+    int RequestData(vtkInformation*, vtkInformationVector**, vtkInformationVector*) override;
+
+private:
+    vtkPolyDataToUnstructuredGrid(const vtkPolyDataToUnstructuredGrid&) = delete;
+    void operator=(const vtkPolyDataToUnstructuredGrid&)                = delete;
+};
+
+//------------------------------------------------------------------------------
+vtkStandardNewMacro(vtkPolyDataToUnstructuredGrid);
+
+//------------------------------------------------------------------------------
+vtkPolyDataToUnstructuredGrid::vtkPolyDataToUnstructuredGrid() = default;
+
+//------------------------------------------------------------------------------
+vtkPolyDataToUnstructuredGrid::~vtkPolyDataToUnstructuredGrid() = default;
+
+//------------------------------------------------------------------------------
+void vtkPolyDataToUnstructuredGrid::PrintSelf(ostream& os, vtkIndent indent)
+{
+    this->Superclass::PrintSelf(os, indent);
+}
+
+//------------------------------------------------------------------------------
+int vtkPolyDataToUnstructuredGrid::FillInputPortInformation(int vtkNotUsed(port), vtkInformation* info)
+{
+    info->Set(vtkAlgorithm::INPUT_REQUIRED_DATA_TYPE(), "vtkPolyData");
+    return 1;
+}
+
+//------------------------------------------------------------------------------
+bool vtkPolyDataToUnstructuredGrid::CanBeProcessedFast(vtkPolyData* polyData)
+{
+    if (!polyData || polyData->GetNumberOfCells() == 0)
+    {
+        return false;
+    }
+    const vtkIdType numVerts  = polyData->GetNumberOfVerts();
+    const vtkIdType numLines  = polyData->GetNumberOfLines();
+    const vtkIdType numPolys  = polyData->GetNumberOfPolys();
+    const vtkIdType numStrips = polyData->GetNumberOfStrips();
+    const bool hasVerts       = (numVerts > 0);
+    const bool hasLines       = (numLines > 0);
+    const bool hasPolys       = (numPolys > 0);
+    const bool hasStrips      = (numStrips > 0);
+    const bool hasOnlyVerts   = (hasVerts && !hasLines && !hasPolys && !hasStrips);
+    const bool hasOnlyLines   = (!hasVerts && hasLines && !hasPolys && !hasStrips);
+    const bool hasOnlyPolys   = (!hasVerts && !hasLines && hasPolys && !hasStrips);
+    const bool hasOnlyStrips  = (!hasVerts && !hasLines && !hasPolys && hasStrips);
+    return hasOnlyVerts || hasOnlyLines || hasOnlyPolys || hasOnlyStrips;
+}
+
+//------------------------------------------------------------------------------
+namespace {
+struct BuildCellTypesImpl
+{
+    // Given a polyData cell array and a size to type functor, it creates the cell types
+    template <typename CellStateT, typename SizeToTypeFunctor>
+    void operator()(CellStateT& state, vtkUnsignedCharArray* cellTypes, SizeToTypeFunctor&& typer, vtkIdType offset)
+    {
+        const vtkIdType numCells = state.GetNumberOfCells();
+        if (numCells == 0)
+        {
+            return;
+        }
+
+        vtkSMPTools::For(0, numCells,
+            [&](vtkIdType begin, vtkIdType end)
+            {
+                auto types = cellTypes->GetPointer(offset);
+                for (vtkIdType cellId = begin; cellId < end; ++cellId)
+                {
+                    types[cellId] = static_cast<unsigned char>(typer(state.GetCellSize(cellId)));
+                }
+            });
+    }
+};
+
+struct BuildConnectivityImpl
+{
+    template <typename CellStateT>
+    void operator()(CellStateT& state, vtkIdTypeArray* outOffSets, vtkIdTypeArray* outConnectivity, vtkIdType offset, vtkIdType connectivityOffset)
+    {
+        using IdType                     = typename CellStateT::ValueType;
+        const auto inOffsets             = state.GetOffsets();
+        const auto inConnectivity        = state.GetConnectivity();
+        const vtkIdType connectivitySize = inConnectivity->GetNumberOfValues();
+        const vtkIdType numCells         = state.GetNumberOfCells();
+
+        // copy connectivity values
+        vtkSMPTools::For(0, connectivitySize,
+            [&](vtkIdType begin, vtkIdType end)
+            {
+                auto inConnPtr  = inConnectivity->GetPointer(0);
+                auto outConnPtr = outConnectivity->GetPointer(connectivityOffset);
+                std::copy(inConnPtr + begin, inConnPtr + end, outConnPtr + begin);
+            });
+        // transform offset values
+        vtkSMPTools::For(0, numCells,
+            [&](vtkIdType begin, vtkIdType end)
+            {
+                auto inOffPtr  = inOffsets->GetPointer(0);
+                auto outOffPtr = outOffSets->GetPointer(offset);
+                std::transform(inOffPtr + begin, inOffPtr + end, outOffPtr + begin,
+                    [&connectivityOffset](IdType val) -> vtkIdType { return val + connectivityOffset; });
+            });
+    }
+};
+} // end anonymous namespace
+
+//------------------------------------------------------------------------------
+int vtkPolyDataToUnstructuredGrid::RequestData(vtkInformation*, vtkInformationVector** inputVector, vtkInformationVector* outputVector)
+{
+    auto input  = vtkPolyData::GetData(inputVector[0], 0);
+    auto output = vtkUnstructuredGrid::GetData(outputVector, 0);
+
+    if (!input || input->GetNumberOfPoints() == 0)
+    {
+        vtkDebugMacro("No input data.");
+        return 1;
+    }
+
+    // copy points
+    vtkNew<vtkPoints> points;
+    points->ShallowCopy(input->GetPoints());
+    output->SetPoints(points);
+
+    // copy point data
+    output->GetPointData()->ShallowCopy(input->GetPointData());
+
+    const vtkIdType numVerts  = input->GetNumberOfVerts();
+    const vtkIdType numLines  = input->GetNumberOfLines();
+    const vtkIdType numPolys  = input->GetNumberOfPolys();
+    const vtkIdType numStrips = input->GetNumberOfStrips();
+    const vtkIdType numCells  = numVerts + numLines + numPolys + numStrips;
+
+    if (numCells == 0)
+    {
+        return 1;
+    }
+
+    this->UpdateProgress(0.05);
+
+    const bool hasVerts  = (numVerts > 0);
+    const bool hasLines  = (numLines > 0);
+    const bool hasPolys  = (numPolys > 0);
+    const bool hasStrips = (numStrips > 0);
+
+    // construct cell types array
+    vtkNew<vtkUnsignedCharArray> cellTypes;
+    cellTypes->SetNumberOfValues(numCells);
+    vtkIdType offset = 0;
+    if (hasVerts)
+    {
+        input->GetVerts()->Visit(
+            BuildCellTypesImpl {}, cellTypes, [](vtkIdType size) -> VTKCellType { return size == 1 ? VTK_VERTEX : VTK_POLY_VERTEX; }, offset);
+    }
+    if (hasLines)
+    {
+        offset += numVerts;
+        input->GetLines()->Visit(
+            BuildCellTypesImpl {}, cellTypes, [](vtkIdType size) -> VTKCellType { return size == 2 ? VTK_LINE : VTK_POLY_LINE; }, offset);
+    }
+    if (hasPolys)
+    {
+        offset += numLines;
+        input->GetPolys()->Visit(
+            BuildCellTypesImpl {}, cellTypes,
+            [](vtkIdType size) -> VTKCellType
+            {
+                switch (size)
+                {
+                case 3:
+                    return VTK_TRIANGLE;
+                case 4:
+                    return VTK_QUAD;
+                default:
+                    return VTK_POLYGON;
+                }
+            },
+            offset);
+    }
+    if (hasStrips)
+    {
+        offset += numPolys;
+        input->GetStrips()->Visit(
+            BuildCellTypesImpl {}, cellTypes, [](vtkIdType vtkNotUsed(size)) -> VTKCellType { return VTK_TRIANGLE_STRIP; }, offset);
+    }
+    this->UpdateProgress(0.5);
+
+    // check if we can shallow copy only one cell array
+    const bool hasOnlyVerts  = (hasVerts && !hasLines && !hasPolys && !hasStrips);
+    const bool hasOnlyLines  = (!hasVerts && hasLines && !hasPolys && !hasStrips);
+    const bool hasOnlyPolys  = (!hasVerts && !hasLines && hasPolys && !hasStrips);
+    const bool hasOnlyStrips = (!hasVerts && !hasLines && !hasPolys && hasStrips);
+
+    if (hasOnlyVerts)
+    {
+        output->SetCells(cellTypes, input->GetVerts(), nullptr, nullptr);
+    }
+    else if (hasOnlyLines)
+    {
+        output->SetCells(cellTypes, input->GetLines(), nullptr, nullptr);
+    }
+    else if (hasOnlyPolys)
+    {
+        output->SetCells(cellTypes, input->GetPolys(), nullptr, nullptr);
+    }
+    else if (hasOnlyStrips)
+    {
+        output->SetCells(cellTypes, input->GetStrips(), nullptr, nullptr);
+    }
+    else
+    {
+        // create offset array
+        vtkNew<vtkIdTypeArray> offsets;
+        offsets->SetNumberOfValues(numCells + 1);
+
+        // create connectivity array
+        vtkNew<vtkIdTypeArray> connectivity;
+        const vtkIdType numConnectivity = input->GetVerts()->GetNumberOfConnectivityIds() + input->GetLines()->GetNumberOfConnectivityIds()
+            + input->GetPolys()->GetNumberOfConnectivityIds() + input->GetStrips()->GetNumberOfConnectivityIds();
+        connectivity->SetNumberOfValues(numConnectivity);
+
+        offset                       = 0;
+        vtkIdType connectivityOffset = 0;
+        if (hasVerts)
+        {
+            input->GetVerts()->Visit(BuildConnectivityImpl {}, offsets, connectivity, offset, connectivityOffset);
+        }
+        if (hasLines)
+        {
+            offset += numVerts;
+            connectivityOffset += input->GetVerts()->GetNumberOfConnectivityIds();
+            input->GetLines()->Visit(BuildConnectivityImpl {}, offsets, connectivity, offset, connectivityOffset);
+        }
+        if (hasPolys)
+        {
+            offset += numLines;
+            connectivityOffset += input->GetLines()->GetNumberOfConnectivityIds();
+            input->GetPolys()->Visit(BuildConnectivityImpl {}, offsets, connectivity, offset, connectivityOffset);
+        }
+        if (hasStrips)
+        {
+            offset += numPolys;
+            connectivityOffset += input->GetPolys()->GetNumberOfConnectivityIds();
+            input->GetStrips()->Visit(BuildConnectivityImpl {}, offsets, connectivity, offset, connectivityOffset);
+        }
+        // set last offset
+        offsets->SetValue(numCells, numConnectivity);
+
+        // create cell array
+        vtkNew<vtkCellArray> cellArray;
+        cellArray->SetData(offsets, connectivity);
+        // set cells
+        output->SetCells(cellTypes, cellArray, nullptr, nullptr);
+    }
+
+    this->UpdateProgress(0.95);
+
+    // copy cell data
+    output->GetCellData()->ShallowCopy(input->GetCellData());
+    this->UpdateProgress(1.0);
+
+    return 1;
+}
 
 int main()
 {
-    std::cout << "please see 13_filter\n";
+    vtkNew<vtkPoints> points;
+    points->InsertNextPoint(0, 0, 0);
+    points->InsertNextPoint(2, 0, 0);
+    points->InsertNextPoint(1, 2, 0);
+    points->InsertNextPoint(0, 2, 0);
+    points->InsertNextPoint(2, 2, 0);
 
-    // 13_filter TEST601 vtkDataSetSurfaceFilter      将 vtkUnstructuredGrid 转换为 vtkPolyData
-    // 13_filter TEST603 vtkDataSetToDataObjectFilter 将数据集(vtkDataSet)转换为数据对象(vtkDataObject)，然后写入文本文件
+    vtkNew<vtkCellArray> polys;
+    polys->InsertNextCell({ 0, 1, 2 });
 
-    // vtkPolyDataToUnstructuredGrid
+    vtkNew<vtkCellArray> lines;
+    lines->InsertNextCell({ 3,4 });
+
+    vtkNew<vtkPolyData> polyData;
+    polyData->SetPoints(points);
+    polyData->SetPolys(polys);
+    polyData->SetLines(lines);
+
+    vtkNew<vtkPolyDataToUnstructuredGrid> poly2uns;
+    poly2uns->SetInputData(polyData);
+    poly2uns->Update();
+
+    poly2uns->GetOutput()->Print(std::cout);
+
+    vtkNew<vtkDataSetMapper> mapper;
+    mapper->SetInputData(poly2uns->GetOutput());
+
+    vtkNew<vtkActor> actor;
+    actor->SetMapper(mapper);
+
+    vtkNew<vtkRenderer> renderer;
+    renderer->AddActor(actor);
+    renderer->SetBackground(.1, .2, .3);
+    renderer->ResetCamera();
+
+    vtkNew<vtkRenderWindow> renderWindow;
+    renderWindow->AddRenderer(renderer);
+
+    vtkNew<vtkRenderWindowInteractor> renderWindowInteractor;
+    renderWindowInteractor->SetRenderWindow(renderWindow);
+
+    vtkNew<vtkInteractorStyleTrackballCamera> style;
+    renderWindowInteractor->SetInteractorStyle(style);
+
+    renderWindow->SetSize(800, 600);
+    renderWindow->Render();
+    renderWindowInteractor->Start();
 }
 
 #endif // TEST401
--- source/01vtkRenderWindow/16_geometry/main.cpp
+++ source/01vtkRenderWindow/16_geometry/main.cpp
@@ -2,11 +2,13 @@
 /**
  * 101. vtkCubeSource 立方体
  * 102. vtkDiskSource 圆盘
+ * 103. vtkRegularPolygonSource 圆
+ * 104. vtkArrowSource 箭头
  * 201. vtkParametricSuperEllipsoid 超椭球体
  *
  */
 
-#define TEST102
+#define TEST104
 
 #ifdef TEST101
 
@@ -103,6 +105,108 @@ int main(int, char*[])
 }
 
 #endif // TEST102
+
+#ifdef TEST103
+
+#include <vtkActor.h>
+#include <vtkInteractorStyleTrackballCamera.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkProperty.h>
+#include <vtkRegularPolygonSource.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkRenderer.h>
+#include <vtkSmartPointer.h>
+
+int main(int, char*[])
+{
+    vtkNew<vtkRegularPolygonSource> source;
+    source->SetNumberOfSides(10); // 设置边数，值越大越接近圆形
+    source->SetRadius(5.0);       // 设置半径
+    source->GeneratePolygonOff(); // 不生成多边形，默认开启
+    source->GeneratePolylineOn(); // 生成圆圈边框
+    source->Update();
+
+    vtkNew<vtkPolyDataMapper> mapper;
+    mapper->SetInputData(source->GetOutput());
+
+    vtkNew<vtkActor> actor;
+    actor->SetMapper(mapper);
+    actor->GetProperty()->SetColor(0, 1, 0);
+
+    vtkNew<vtkRenderer> renderer;
+    renderer->AddActor(actor);
+    renderer->ResetCamera();
+
+    vtkNew<vtkRenderWindow> renWin;
+    renWin->AddRenderer(renderer);
+    renWin->SetSize(800, 600);
+
+    vtkNew<vtkRenderWindowInteractor> iren;
+    iren->SetRenderWindow(renWin);
+
+    vtkNew<vtkInteractorStyleTrackballCamera> style;
+    iren->SetInteractorStyle(style);
+
+    renWin->Render();
+    iren->Start();
+
+    return 0;
+}
+
+#endif // TEST103
+
+#ifdef TEST104
+
+#include <vtkActor.h>
+#include <vtkArrowSource.h>
+#include <vtkInteractorStyleTrackballCamera.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkProperty.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkRenderer.h>
+#include <vtkSmartPointer.h>
+
+int main(int, char*[])
+{
+    vtkNew<vtkArrowSource> source;
+    source->SetShaftRadius(.1);     // 轴半径
+    source->SetShaftResolution(10); // 轴分辨率
+    source->SetTipResolution(10);   // 尖端分辨率
+    source->SetTipLength(.3);       // 尖端高度，轴的长度为1
+    source->SetTipRadius(.2);       // 尖端半径
+    source->SetInvert(false);       // 是否翻转
+    source->Update();
+
+    vtkNew<vtkPolyDataMapper> mapper;
+    mapper->SetInputData(source->GetOutput());
+
+    vtkNew<vtkActor> actor;
+    actor->SetMapper(mapper);
+    actor->GetProperty()->SetColor(0, 1, 0);
+
+    vtkNew<vtkRenderer> renderer;
+    renderer->AddActor(actor);
+    renderer->ResetCamera();
+
+    vtkNew<vtkRenderWindow> renWin;
+    renWin->AddRenderer(renderer);
+    renWin->SetSize(800, 600);
+
+    vtkNew<vtkRenderWindowInteractor> iren;
+    iren->SetRenderWindow(renWin);
+
+    vtkNew<vtkInteractorStyleTrackballCamera> style;
+    iren->SetInteractorStyle(style);
+
+    renWin->Render();
+    iren->Start();
+
+    return 0;
+}
+
+#endif // TEST103
 
 #ifdef TEST201
 
